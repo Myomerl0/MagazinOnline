4639a2734d44bb7d745f705e0661871b28b89f2a
Myomerl0 <matei.craciun@student.upt.ro>
2025-01-16 00:06:53 +0200

    Magazin

---
+++ Magazin.cs
@@ -0,0 +1,12 @@
+﻿namespace MagazinOnline;
+
+public class Magazin
+{
+    public List<ProdusGeneric> Produse { get;private set; }
+    public string Nume { get; private set; }
+    public Magazin(string name)
+    {
+        Nume = name;
+        Produse = new List<ProdusGeneric>();
+    }
+}
