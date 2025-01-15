a26bfe1557b9e8f0e333511a5d3baeaeefbd3bae
AnaGoga <anagoga248@gmail.com>
2025-01-16 00:02:06 +0200

    Cos

---
+++ Coscs.cs
@@ -0,0 +1,27 @@
+﻿namespace MagazinOnline;
+
+public class Cos
+{
+    public List<string> CosProd { get; set; }
+    public Cos()
+    {
+        CosProd = new List<string>();
+    }
+    public void AdaugareInCos(string prod)
+    {
+        CosProd.Add(prod);
+    }
+    public void Golire()
+    {
+        CosProd.Clear();
+    }
+    public override string ToString()
+    {
+        string afisare = "Cos: ";
+        foreach (var prod in CosProd)
+        {
+            afisare = afisare + prod + ' , ';
+        }
+        return afisare;
+    }
+}
