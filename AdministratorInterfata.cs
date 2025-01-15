6b7b3c8dbfdd2e9cec9b59edff9bc083a5d6f6a2
Myomerl0 <matei.craciun@student.upt.ro>
2025-01-15 18:46:04 +0200

    Add project files.

---
+++ AdministratorInterfata.cs
@@ -0,0 +1,14 @@
+﻿namespace MagazinOnline;
+
+public interface Administrator
+{
+    public void AddProdusGeneric(ProdusGeneric prouds);
+    public void AddProdusPerisabil(ProdusPerisabil produs);
+    public void AddProdusElectrocasnic(ProdusElectrocasnic produs);
+    public void ScoateProdusStoc(string NumeProdus);
+    public void ASnumarBucati(string NumeProdus, int adaugare);
+    public void VizualizareProduse();
+    public void ModificareStatus(int NumarulComenzii, bool valid);
+    public void ModificareDataLiv(int NumarulComenz, DateTime dataNoua);
+    public void AdaugareComandaNoua(Comanda ComandaNoua);
+}
