51de14f3d56efe8c7c5434597d0ffd02e994f3c1
AnaGoga <anagoga248@gmail.com>
2025-01-16 00:03:55 +0200

    Electrocasnic + Perisabil

---
+++ ProdElec.cs
@@ -0,0 +1,16 @@
+﻿namespace MagazinOnline;
+
+public class ProdusElectrocasnic : ProdusGeneric
+{
+    public string EficientaEn { get; private set; }
+    public int ConsumMax {  get; private set; }
+    public ProdusElectrocasnic(string Nume,decimal Pret,int Stoc, string EficientaEn,int ConsumMax):base(Nume,Pret,Stoc)
+    {
+        this.EficientaEn=EficientaEn;
+        this.ConsumMax=ConsumMax;
+    }
+    public override string ToString()
+    {
+        return $"Produsul electro {Nume}, Euro: {Pret}, {Stoc} ramase in stoc, eficienta energetica{EficientaEn}, Consum maxim{ConsumMax} W";
+    }
+}
---
+++ ProdPers.cs
@@ -0,0 +1,15 @@
+﻿namespace MagazinOnline;
+public class ProdusPerisabil : ProdusGeneric
+{ 
+    public string Conditii {  get;private set; }
+    public DateTime Expirare { get; private set; }
+    public ProdusPerisabil(string Nume,decimal Pret,int Stoc,DateTime Expirare,string Conditii):base(Nume,Pret, Stoc)
+    {   
+        this.Expirare = Expirare;
+        this.Conditii = Conditii;
+    }
+    public override string ToString()
+    {
+        return $"Produsul electro {Nume}, Euro: {Pret}, {Stoc} ramase in stoc, conditii de tinere {Conditii}, data expirare{Expirare}";
+    }
+}
