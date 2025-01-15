d922a0fb98badae7c4dcc66a306b1b0be37d65c7
Myomerl0 <matei.craciun@student.upt.ro>
2025-01-16 00:08:29 +0200

    Configurari(PtValidari)

---
+++ Configuratii.cs
@@ -0,0 +1,10 @@
+﻿namespace MagazinOnline;
+
+public static class Configurare
+{
+    public const int LenAdresaMax = 10;
+    public const int StocMax = 1000;
+    public const int AniExpirareMax = 3;
+    public const decimal PretMax = 1000000000;
+    public int PutereElectronicMax = 4000;
+}
